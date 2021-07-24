2c7b397461d2809402e718076a6b3a09f7ab244e
YSG <qpple6@naver.com>
2021-07-25 00:56:18 +0900

    seed추가 및 Palette 컴포넌트 생성

---
+++ src/components/Palette.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Palette({palette}) {
+  return (
+    <div className="Palette">
+      {/* Navbar */}
+      <div className="Palette-colors">
+      
+      </div>
+      {/* footer */}
+    </div>
+  )
+}
+
+export default Palette
