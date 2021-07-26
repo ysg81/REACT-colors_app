bd4b0cc78887435066e4278b21355e987425e94e
YSG <qpple6@naver.com>
2021-07-27 00:00:29 +0900

    chroma를 이용한 ColorHelper 추가

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Palette from './components/Palette';
 import seedColors from './seedColors'
+import { generatePalette } from './colorHelper'
 
 function App() {
+  console.log(generatePalette(seedColors[4]))
   return (
     <div>
       <Palette palette={seedColors[4]}/>
